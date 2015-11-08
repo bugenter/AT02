82f7ce31a63f4de8b72784d9bf60bfa5e7b97832
phamtham90913 <nguyenpham90913@gmail.com>
2015-11-08 16:51:05 +0700

    datadriven

--- src/test/java/mavenPackage/TestLogin.java
+++ src/test/java/mavenPackage/TestLogin.java
@@ -1,5 +1,7 @@
 package mavenPackage;
 
+import java.io.File;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,12 +14,13 @@ import common.ExcelUtils;
 import common.LoginPage;
 
 public class TestLogin extends LoginPage {
-
+	String path;
 	@BeforeMethod
 	public void before(){
 		driver = new FirefoxDriver();
 		driver.get(Constant.homePageUrl);
-		 wait = new WebDriverWait(driver,30);
+		wait = new WebDriverWait(driver,30);
+		path = (System.getProperty("user.dir")+"/src/main/java/data/").replace("/", File.separator).replace("\\", File.separator);
 	}
 	@AfterMethod
 	public void after(){
@@ -25,7 +28,8 @@ public class TestLogin extends LoginPage {
 	}
 	@Test (priority =0)
 	public void loginSuccessfully() throws Exception{
-		ExcelUtils.setExcelFile(Constant.Path_TestData+Constant.File_TestData, "Login");
+		System.out.println(path);
+		ExcelUtils.setExcelFile(path+Constant.File_TestData, "Login");
 		String email= ExcelUtils.getCellData(1, 0);
 		String pass= ExcelUtils.getCellData(1, 1);
 		gotoLoginPage();
@@ -33,11 +37,11 @@ public class TestLogin extends LoginPage {
 		driver.findElement(E_BTN_LOGIN).click();
 		wait.until(ExpectedConditions.visibilityOfElementLocated(E_DASHBOARD));
 		driver.findElement(E_DASHBOARD);
-		
+
 	}
 	@Test (priority =1)
 	public void loginFail() throws Exception{
-		ExcelUtils.setExcelFile(Constant.Path_TestData+Constant.File_TestData, "Login");
+		ExcelUtils.setExcelFile(path+Constant.File_TestData, "Login");
 		String email= ExcelUtils.getCellData(2, 0);
 		String pass= ExcelUtils.getCellData(2, 1);
 		gotoLoginPage();
