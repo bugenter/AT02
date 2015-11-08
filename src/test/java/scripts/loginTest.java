92634812fbdd2eb30167e34df81dd0ba1209d977
unknown <phuongungson@gmail.com>
2015-11-08 17:10:02 +0700

    test

--- src/test/java/scripts/loginTest.java
+++ src/test/java/scripts/loginTest.java
@@ -1,13 +1,16 @@
 package scripts;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import common.homePage;
 import common.loginPage;
+import datadriven.Constant;
+import datadriven.ExcelUtils;
 
 
 public class loginTest {
@@ -15,7 +18,7 @@ public class loginTest {
 	homePage home;
 	loginPage log;
 
-	@BeforeMethod
+	@BeforeMethod(groups={"testlogin"})
 	public void before(){
 		WebDriver dr = new FirefoxDriver();		
 		home = new homePage(dr);
@@ -24,16 +27,25 @@ public class loginTest {
 		log.gotoLogin();
 	}
 
-	@Test
-	public void testLogin(){
-		log.inputLogin("phuong@abc.com", "111111");
-		log.verifyLogin(true);
+	@Test(groups={"testlogin"})
+	public void testLogin() throws Exception{
+		ExcelUtils.setExcelFile(Constant.Path_TestData + Constant.File_TestData,Constant.sheetName);
+		String email= ExcelUtils.getCellData(2, 2);
+		String pass= ExcelUtils.getCellData(2, 3);
+		log.inputLogin(email, pass);
+
+		dr.findElement(By.xpath(".//a/*[text()='Account']")).click();
+		dr.findElement(By.xpath(".//*[@id='header-account']//*[contains(text(),'My Cart')]")).click();
+		WebElement e = dr.findElement(By.id("empty_cart_button"));
+		if (e!=null)
+			e.click();
 	}
 
-	@AfterMethod
+	@AfterMethod(groups={"testlogin"})
 	public void after(){
 		//		home.logOut();
-		home.closeBrowser();
+		//home.closeBrowser();
 	}
+
 
 }
